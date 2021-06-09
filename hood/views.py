31f8d54f2171bab412e36f8f25d0351727fa304b
mahan-noor <ismahanabey@gmail.com>
2021-06-09 13:02:26 +0300

    create templates and index.html and a function that gets all neighbourhood

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http  import HttpResponse,Http404
+from django.shortcuts import render,redirect
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    neighborhoods = Neighborhood.get_all_neighborhoods()
+    return render(request, 'index.html',{"neighborhoods":neighborhoods})
