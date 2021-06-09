e99c1de82d4b5a50e48da32630daa5c45e59d77e
mahan-noor <ismahanabey@gmail.com>
2021-06-09 14:53:10 +0300

    add post class in the models and add save,delete and get method

--- hood/models.py
+++ hood/models.py
@@ -12,6 +12,8 @@ class Neighborhood(models.Model):
     neighborhood_location = models.CharField(max_length=30)
     neighborhood_pic = CloudinaryField("1920x1080")
     occupants_count = models.IntegerField(null=True)
+    police_contact = PhoneNumberField()
+    health_contact = PhoneNumberField()
 
     @classmethod
     def get_all_neighborhoods(cls):
@@ -25,3 +27,35 @@ class Profile(models.Model):
     email = models.EmailField()
     profile_pic = CloudinaryField('1080x1080')
     bio = HTMLField()
+
+
+    def save_bs(self):
+        self.save()
+
+    def delete_bs(self):
+        self.delete()
+
+    @classmethod
+    def get_all_bs(cls):
+        businesses = cls.objects.all()
+        return businesses
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    description = HTMLField(max_length=70)
+    post_user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+    post_location = models.ForeignKey(Neighborhood, related_name="posts", on_delete=models.CASCADE)
+    post_pic = CloudinaryField('1920x1080')
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def get_all_posts(cls):
+        posts = cls.objects.all()
+        return posts
+
