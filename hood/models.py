4dd225328e342c4e9a243facf150dbdbb61effb0
mahan-noor <ismahanabey@gmail.com>
2021-06-09 13:51:42 +0300

    add neighborhood model

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+
 
 # Create your models here.
+class Neighborhood(models.Model):
+    neighborhood_name = models.CharField(max_length=30)
+    neighborhood_location = models.CharField(max_length=30)
+    neighborhood_pic = ImageField(blank=True, manual_crop="1920x1080")
+    occupants_count = models.IntegerField(null=True)
