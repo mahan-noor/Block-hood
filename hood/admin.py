4bf6cf47e1a4dc703d72cfef094b7cd6c71c4736
mahan-noor <ismahanabey@gmail.com>
2021-06-09 15:57:20 +0300

    create navbar file also register model classes in the admin page

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Neighborhood,Profile,Business,Post
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    filter_horizontal =('neighborhood',)
+
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
