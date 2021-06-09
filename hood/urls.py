3af9c37c2b1afa5979cc9a7f18588ebffca4d42b
mahan-noor <ismahanabey@gmail.com>
2021-06-09 17:07:40 +0300

    add url path of django registration to allow users to login and register successfully

--- hood/urls.py
+++ hood/urls.py
@@ -13,6 +13,7 @@ urlpatterns=[
     path('new/business', views.new_business, name='new-business')
  
 ]
+
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
