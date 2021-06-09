7e95b346dcbf914512e7f57b55f85d810593b857
mahan-noor <ismahanabey@gmail.com>
2021-06-10 01:32:41 +0300

    deployment

--- myblock/settings.py
+++ myblock/settings.py
@@ -34,11 +34,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 # Application definition
