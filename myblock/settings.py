7f4729ac05e2483e7e91576c28d3af71f3a98467
mahan-noor <ismahanabey@gmail.com>
2021-06-10 01:29:44 +0300

    deployment

--- myblock/settings.py
+++ myblock/settings.py
@@ -32,13 +32,15 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
 # Application definition
 
 INSTALLED_APPS = [
