90c651759e422142a27d8c9a9e4b28fba25f7f74
mahan-noor <ismahanabey@gmail.com>
2021-06-09 13:59:04 +0300

    add model neighborhood to views.py and run migration

--- hood/models.py
+++ hood/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 
 
@@ -8,5 +10,5 @@ from django.contrib.auth.models import User
 class Neighborhood(models.Model):
     neighborhood_name = models.CharField(max_length=30)
     neighborhood_location = models.CharField(max_length=30)
-    neighborhood_pic = ImageField(blank=True, manual_crop="1920x1080")
+    neighborhood_pic = CloudinaryField("1920x1080")
     occupants_count = models.IntegerField(null=True)
--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse,Http404
-from django.shortcuts import render,redirect
+from django.shortcuts import redirect
+from .models import Neighborhood
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
