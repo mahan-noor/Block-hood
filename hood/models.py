df4c5f408e1a60220d2aeae4db4c4bc85e2cf14f
mahan-noor <ismahanabey@gmail.com>
2021-06-09 14:15:51 +0300

    add class profile and migrate it to the database

--- hood/models.py
+++ hood/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-
-
-
+from tinymce.models import HTMLField
 
 
 # Create your models here.
@@ -12,3 +10,16 @@ class Neighborhood(models.Model):
     neighborhood_location = models.CharField(max_length=30)
     neighborhood_pic = CloudinaryField("1920x1080")
     occupants_count = models.IntegerField(null=True)
+
+    @classmethod
+    def get_all_neighborhoods(cls):
+        neighborhoods = cls.objects.all()
+        return neighborhoods
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+    neighborhood = models.ManyToManyField(Neighborhood)
+    email = models.EmailField()
+    profile_pic = CloudinaryField('1080x1080')
+    bio = HTMLField()
